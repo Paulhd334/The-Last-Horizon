4d496bdf36631aee44309d1f75fcd123c9e2beec
Paulhd334 <76810173+Paulhd334@users.noreply.github.com>
2024-12-31 15:13:53 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,15 +1,1 @@
-const cookieBox = document.querySelector(".wrapper"),
-  buttons = document.querySelectorAll(".button");
 
-const executeCodes = () => {
-  cookieBox.classList.add("show");
-
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      cookieBox.classList.remove("show");
-    });
-  });
-};
-
-// executeCodes function will be called on webpage load
-window.addEventListener("load", executeCodes);
